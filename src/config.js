77c9a3f6cc9de8ac9af7d08231a3040792848990
igorovh <animekgaming@gmail.com>
2023-11-08 18:17:08 +0100

    Fix config error

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
-export const config = await Bun.file('./config.json').json();
+import fs from 'fs';
+
+// export const config = await Bun.file('../config.json').json(); // Bad file descriptor error
+export const config = JSON.parse(fs.readFileSync('./config.json'));
 
 export const getChannels = () => config.map(log => log.channel);
